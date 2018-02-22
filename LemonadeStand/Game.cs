de828934299a16587f24479af75a7512fbda9a4c
MeredithA <meredith.albrecht00@gmail.com>
2018-02-22 15:26:07 -0600

    Rough outline

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+        // member variables (HAS A)
+
+
+
+        // constructor ()
+
+
+        //member methods (CAN DO)
+        public void RunGme()
+        {
+            //Weather
+                 //dailyForcat - (DisplayDailyForcast)
+                 // Weekly Forcast - (DisplayWeeklyForcast
+
+                    //SetPrice per cup - (SetPriceBasedOnWeather)
+                 
+             //player(BONUS SECOND PLAYER)
+                //NameOfStand
+
+              //ChangeRecipe
+                //lemons
+                //sugar
+                //ice
+
+             // Customer {make list?}     (each customer separate object)
+                                        //their own chance of buyins a glass
+                                        //so thathow much lemonade's purchased
+                                        //how much their willing to pay (vary form CX to CX)
+             // Profit
+                //calulate profit or loss at the end of the day
+                //keep running total.
+
+            //loop at least 7 times  (((option to choose (7, 14, 21 days)
+
+            // Retire or Start over
+        }
+
+        public void CreateRecipe()
+        {
+
+        }
+
+    }
+}
